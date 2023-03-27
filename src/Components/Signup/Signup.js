8bee3cc49b302875d91f0e9752a8b637a4efd4f5
Ragesh-A <ragesha2017@gmail.com>
2023-03-27 10:51:13 +0530

    Sign up setup

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,59 +1,63 @@
-import React from 'react';
-
+import React, { useState, useContext } from 'react';
+import 'firebase/auth'
 import Logo from '../../olx-logo.png';
+import { FirebaseContext } from '../../store/FirebaseContext';
 import './Signup.css';
+import { useHistory } from 'react-router-dom'
 
 export default function Signup() {
-  return (
-    <div>
-      <div className="signupParentDiv">
-        <img width="200px" height="200px" src={Logo}></img>
-        <form>
-          <label htmlFor="fname">Username</label>
-          <br />
-          <input
-            className="input"
-            type="text"
-            id="fname"
-            name="name"
-            defaultValue="John"
-          />
-          <br />
-          <label htmlFor="fname">Email</label>
-          <br />
-          <input
-            className="input"
-            type="email"
-            id="fname"
-            name="email"
-            defaultValue="John"
-          />
-          <br />
-          <label htmlFor="lname">Phone</label>
-          <br />
-          <input
-            className="input"
-            type="number"
-            id="lname"
-            name="phone"
-            defaultValue="Doe"
-          />
-          <br />
-          <label htmlFor="lname">Password</label>
-          <br />
-          <input
-            className="input"
-            type="password"
-            id="lname"
-            name="password"
-            defaultValue="Doe"
-          />
-          <br />
-          <br />
-          <button>Signup</button>
-        </form>
-        <a>Login</a>
-      </div>
-    </div>
-  );
+
+const history = useHistory()
+const [username, setUsername] = useState('');
+const [email, setEmail] = useState('');
+const [phone, setPhone] = useState('');
+const [password, setPassword] = useState('');
+const [err, setErr] = useState('');
+const {firebase} = useContext(FirebaseContext)
+const handleSubmit = (e) =>{
+  e.preventDefault();
+   firebase.auth().createUserWithEmailAndPassword(email, password).then(result=>{
+    result.user.updateProfile({displayName : username}).then(()=>{
+      firebase.firestore().collection('users').add({
+        id: result.user.uid,
+        username : username,
+        phone : phone,
+      }).then(()=>{
+        history.push('/login')
+      })
+    })
+   }).catch(error=>{
+    setErr(`${error.message}`)
+    console.log('new', err);
+   })
+}
+return (
+<div>
+  <div className="signupParentDiv">
+    <img width="200px" height="200px" src={Logo} alt='logo' />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Username</label>
+      <br />
+      <input className="input" type="text" id="name" value={username} onChange={(e)=>setUsername(e.target.value)} name="name"/>
+      <br />
+      <label htmlFor="email">Email</label>
+      <br />
+      <input className="input" type="email" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+      <br />
+      <label htmlFor="phone">Phone</label>
+      <br />
+      <input className="input" type="number" id="phone" name="phone" value={phone} onChange={e=>setPhone(e.target.value)} />
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input className="input" type="password" id="password" name="password"  onChange={e=>setPassword(e.target.value)} />
+      <br />
+      <br />
+      <p className='red'>{err}</p>
+      <button>Signup</button>
+    </form>
+    <a>Login</a>
+  </div>
+</div>
+);
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/auth'
+import 'firebase/firebase'
+import env from '../env.js'
+
+const firebaseConfig = {
+  apiKey: env.apiKey,
+  authDomain: env.authDomain,
+  projectId: env.projectId,
+  storageBucket: env.storageBucket,
+  messagingSenderId: env.messagingSenderId,
+  appId: env.appId,
+  measurementId: env.measurementId
+};
+
+console.log(env.apiKey);
+
+export default firebase.initializeApp(firebaseConfig);
